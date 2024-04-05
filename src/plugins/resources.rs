63e303462729184f1e02b891e8c35c127f87bce6
Skippy <steelstone3@gmail.com>
2024-04-05 18:50:53 +0100

    update

--- src/plugins/resources.rs
+++ src/plugins/resources.rs
@@ -1,15 +1,11 @@
 use bevy::prelude::{App, Plugin};
 
+use crate::resources::tile_size::TileSize;
+
 pub struct ResourcesPlugin;
 
 impl Plugin for ResourcesPlugin {
-    fn build(&self, _app: &mut App) {
-        // app.insert_resource(WindowSize::default())
-        //     .insert_resource(Points::default())
-        //     .insert_resource(LaserAmmunition(10.0))
-        //     .insert_resource(TorpedoAmmunition(3.0))
-        //     .insert_resource(SelectedWeapon(1))
-        //     .insert_resource(EnemyCount(Default::default()))
-        //     .insert_resource(EnemyLimit(1));
+    fn build(&self, app: &mut App) {
+        app.insert_resource(TileSize { size: 32.0 });
     }
 }
--- src/resources/tile_size.rs
+++ src/resources/tile_size.rs
@@ -1,4 +1,6 @@
 use bevy::ecs::system::Resource;
 
 #[derive(Resource)]
-pub struct TileSize {}
+pub struct TileSize {
+    pub size: f32,
+}
