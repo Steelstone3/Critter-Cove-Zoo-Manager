0a92de6c1e14d73eeec6ca11e18ed4eeb261ab97
Skippy <steelstone3@gmail.com>
2024-04-05 19:13:14 +0100

    Update animal.rs

--- src/assets/images/animal.rs
+++ src/assets/images/animal.rs
@@ -3,16 +3,12 @@ use std::fmt::Display;
 
 #[derive(RandGen)]
 pub enum GameAnimal {
-    AnimatedBoar,
     Boar,
 }
 
 impl Display for GameAnimal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            GameAnimal::AnimatedBoar => {
-                write!(f, "images/animals/zoo/boar.gif")
-            }
             GameAnimal::Boar => {
                 write!(f, "images/animals/zoo/boar.png")
             }
