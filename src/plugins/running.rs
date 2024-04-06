492694445421e801f9492e4779a0c392600a1db1
HoppySophie <82888894+HoppySophie@users.noreply.github.com>
2024-04-06 22:09:10 +0100

    Camera movement

--- src/plugins/running.rs
+++ src/plugins/running.rs
@@ -1,10 +1,14 @@
 use crate::systems::{
-    animate_sprites::animate_sprites, camera::camera_movement, play_music::play_music, user_interface::{
+    animate_sprites::animate_sprites,
+    camera::camera_movement::camera_movement,
+    play_music::play_music,
+    user_interface::{
         menu_selection::menu_selection, update_user_interface::update_user_interface,
-    }
+    },
 };
 use bevy::{
-    app::Update, ecs::schedule, prelude::{App, Plugin}
+    app::Update,
+    prelude::{App, Plugin},
 };
 
 pub struct RunningPlugin;
@@ -14,7 +18,7 @@ impl Plugin for RunningPlugin {
         app.add_systems(Update, play_music)
             .add_systems(Update, menu_selection)
             .add_systems(Update, update_user_interface)
-            .add_systems(Update, animate_sprites) 
+            .add_systems(Update, animate_sprites)
             .add_systems(Update, camera_movement);
     }
 }
--- src/systems/camera/camera_movement.rs
+++ src/systems/camera/camera_movement.rs
@@ -1,15 +1,29 @@
+use bevy::{
+    ecs::system::{Query, Res},
+    input::{keyboard::KeyCode, ButtonInput},
+    render::camera::Camera,
+    time::Time,
+    transform::components::Transform,
+};
 
-
-pub fn camera_movement( 
+pub fn camera_movement(
     input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
-    mut camera: Query<(&mut Transform, &mut Camera)>
+    mut camera: Query<(&mut Transform, &mut Camera)>,
 ) {
-    let movement = time.delta_seconds();
+    let Ok((mut transform, _)) = camera.get_single_mut() else {
+        return;
+    };
+
+    let movement = 100.0 * time.delta_seconds();
     if input.pressed(KeyCode::KeyW) {
-        camera.0.y + movement;   
+        transform.translation.y += movement;
+    } else if input.pressed(KeyCode::KeyS) {
+        transform.translation.y -= movement;
     }
-    else if input.pressed(KeyCode::KeyS){
-        camera.0.y - movement;
+    if input.pressed(KeyCode::KeyA) {
+        transform.translation.x -= movement;
+    } else if input.pressed(KeyCode::KeyD) {
+        transform.translation.x += movement;
     }
 }
