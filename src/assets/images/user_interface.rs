7482faa75c08cc47ea0307ceaf8df153a5e34f40
Skippy <steelstone3@gmail.com>
2024-04-06 21:34:51 +0100

    update

--- src/assets/images/user_interface.rs
+++ src/assets/images/user_interface.rs
@@ -9,10 +9,10 @@ impl Display for MainMenuUserInterface {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             MainMenuUserInterface::IconAnimals => {
-                write!(f, "images/user_interface/animals_icon.png")
+                write!(f, "images/user_interface/main_menu/animals_icon.png")
             }
             MainMenuUserInterface::SelectedIconAnimals => {
-                write!(f, "images/user_interface/selected_animals_icon.png")
+                write!(f, "images/user_interface/main_menu/selected_animals_icon.png")
             }
         }
     }
