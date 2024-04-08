30bd7e73d9e4f00597b30ec696d8549e1fe2d653
Skippy <steelstone3@gmail.com>
2024-04-08 19:29:05 +0100

    update

--- src/systems/spawn_animal.rs
+++ src/systems/spawn_animal.rs
@@ -15,26 +15,20 @@ use bevy::{
         mouse::{MouseButton, MouseButtonInput},
         ButtonState,
     },
-    math::Vec3,
     transform::components::Transform,
+    utils::tracing,
+    window::CursorMoved,
 };
 
 pub fn spawn_animal(
     mut selected_item: ResMut<SelectedMenuItem>,
     mut mouse_button_input_events: EventReader<MouseButtonInput>,
-    // _cursor_moved_events: EventReader<CursorMoved>,
+    mut cursor_moved_events: EventReader<CursorMoved>,
     mut spawn_animated_sprite_event: EventWriter<SpawnAnimatedSpriteEvent>,
 ) {
     if selected_item.animal_selection == ZooAnimal::None {
         return;
     }
-
-    // let mut cursor_position = Vec3::new(0.0, 0.0, 1.0);
-
-    // for cursor_moved_event in cursor_moved_events.read() {
-    //     cursor_position.x = cursor_moved_event.position.x;
-    //     cursor_position.y = cursor_moved_event.position.y;
-    // }
 
     for mouse_button_event in mouse_button_input_events.read() {
         if mouse_button_event.button != MouseButton::Left {
@@ -54,6 +48,18 @@ pub fn spawn_animal(
             animal = Animal::new_32(selected_item.animal_selection)
         }
 
+        let mut transform = Transform::default();
+        transform.translation.z = 1.0;
+
+        for cursor_moved_event in cursor_moved_events.read() {
+            tracing::info!("cursor at {:?}", cursor_moved_event);
+
+            transform.translation.x = cursor_moved_event.position.x;
+            transform.translation.y = cursor_moved_event.position.y;
+        }
+
+        tracing::info!("animal at {:?}", transform.translation);
+
         spawn_animated_sprite_event.send(SpawnAnimatedSpriteEvent {
             frame_timing: animal.frame_timing,
             frame_count: animal.frame_count,
@@ -62,10 +68,7 @@ pub fn spawn_animal(
             spawn_sprite_event: SpawnSpriteEvent {
                 sprite_path: animal.sprite_path.to_string(),
                 size: animal.size,
-                transform: Transform {
-                    translation: Vec3::new(0.0, 0.0, 1.0),
-                    ..Default::default()
-                },
+                transform,
             },
         });
 
