97268ace567cb67b675de44a74cf9e34b224b667
Skippy <steelstone3@gmail.com>
2024-04-08 23:46:21 +0100

    update

--- src/systems/animal_movement.rs
+++ src/systems/animal_movement.rs
@@ -1,9 +1,43 @@
-use bevy::ecs::system::Query;
+use super::controllers::random_generator::{generate_seed, random_value_f32};
+use crate::{
+    assets::images::animal, components::constants::MAP_SIZE, queries::animal_queries::MutableAnimalTransformQuery
+};
+use bevy::{
+    ecs::system::{Query, Res},
+    math::{Quat, Vec3},
+    time::Time, utils::tracing,
+};
 
-use crate::queries::animal_queries::MutableAnimalTransformQuery;
+pub fn animal_movement(mut animal_queries: Query<MutableAnimalTransformQuery>, time: Res<Time>) {
+    for animal_query in &animal_queries  {
+        tracing::info!("Hello1");
+    }
+    
+    animal_queries.par_iter_mut().for_each(|mut animal_query| {
+        tracing::info!("Hello2");
+        let speed = animal_query.animal.speed * time.delta_seconds();
+        let direction = animal_query.animal.direction * Vec3::Y;
+        let translation_delta = direction * speed;
 
-pub fn animal_movement(mut animal_queries: Query<MutableAnimalTransformQuery>) {
-    animal_queries.par_iter_mut().for_each(|_animal_query| {
+        let next_translation_delta = animal_query.transform.translation + translation_delta;
+
+        let is_top_world_border = next_translation_delta.y > MAP_SIZE;
+        let is_right_world_border = next_translation_delta.x > MAP_SIZE;
+        let is_bottom_world_border = next_translation_delta.y < -MAP_SIZE;
+        let is_left_world_border = next_translation_delta.x < -MAP_SIZE;
+
+        if is_top_world_border
+            || is_right_world_border
+            || is_bottom_world_border
+            || is_left_world_border
+        {
+            animal_query.transform.rotate(Quat::from_axis_angle(
+                Vec3::new(0.0, 0.0, 1.0),
+                random_value_f32(generate_seed(), 150.0..210.0),
+            ))
+        } else {
+            animal_query.transform.translation += translation_delta;
+        }
 
         // let ship_speed = ai_ship.velocity * time.delta_seconds();
         // let movement_direction = transform.rotation * Vec3::Y;
--- src/systems/controllers/random_generator.rs
+++ src/systems/controllers/random_generator.rs
@@ -2,21 +2,18 @@ use std::ops::Range;
 
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
-#[allow(dead_code)]
-pub fn random_value_i32(seed: u64, range: Range<i32>) -> i32 {
-    let mut rng = StdRng::seed_from_u64(seed);
+// pub fn random_value_i32(seed: u64, range: Range<i32>) -> i32 {
+//     let mut rng = StdRng::seed_from_u64(seed);
 
-    rng.gen_range(range.start..range.end)
-}
+//     rng.gen_range(range.start..range.end)
+// }
 
-#[allow(dead_code)]
 pub fn random_value_f32(seed: u64, range: Range<f32>) -> f32 {
     let mut rng = StdRng::seed_from_u64(seed);
 
     rng.gen_range(range.start..range.end)
 }
 
-#[allow(dead_code)]
 pub fn generate_seed() -> u64 {
     let mut rng = rand::thread_rng();
 
@@ -27,19 +24,19 @@ pub fn generate_seed() -> u64 {
 mod random_generator_should {
     use super::*;
 
-    #[test]
-    fn generate_a_random_value_i32() {
-        // Given
-        let seed = 1234;
-        let range = -20..20;
-        let expected = -18;
+    // #[test]
+    // fn generate_a_random_value_i32() {
+    //     // Given
+    //     let seed = 1234;
+    //     let range = -20..20;
+    //     let expected = -18;
 
-        // When
-        let actual = random_value_i32(seed, range);
+    //     // When
+    //     let actual = random_value_i32(seed, range);
 
-        // Then
-        assert_eq!(expected, actual);
-    }
+    //     // Then
+    //     assert_eq!(expected, actual);
+    // }
 
     #[test]
     fn generate_a_random_value_f32() {
