4db19faa089fcc9b5540783b002f71b9e1847163
Skippy <steelstone3@gmail.com>
2024-04-09 01:18:54 +0100

    Update animal_movement.rs

--- src/systems/animal_movement.rs
+++ src/systems/animal_movement.rs
@@ -35,22 +35,3 @@ pub fn animal_movement(mut animal_queries: Query<MutableAnimalTransformQuery>, t
         }
     });
 }
-
-// let ship_speed = starship.starship.velocity * time.delta_seconds();
-// let movement_direction = starship.transform.rotation * Vec3::Y;
-// let translation_delta = movement_direction * ship_speed;
-
-// let next_translation = starship.transform.translation + translation_delta;
-
-// if next_translation.y > space_zone_border.top_border
-//     || next_translation.x < space_zone_border.left_border
-//     || next_translation.y < space_zone_border.bottom_border
-//     || next_translation.x > space_zone_border.right_border
-// {
-//     starship.transform.rotate(Quat::from_axis_angle(
-//         Vec3::new(0.0, 0.0, 1.0),
-//         random_value_f32(generate_seed(), 150.0..210.0),
-//     ))
-// } else {
-//     starship.transform.translation += translation_delta;
-// }
