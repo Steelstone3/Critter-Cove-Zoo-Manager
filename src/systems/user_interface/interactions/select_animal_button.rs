25fdadd08f2f8fa5d898e4359362fe07ba989456
HoppySophie <82888894+HoppySophie@users.noreply.github.com>
2024-04-13 16:42:38 +0100

    Update select_animal_button.rs

--- src/systems/user_interface/interactions/select_animal_button.rs
+++ src/systems/user_interface/interactions/select_animal_button.rs
@@ -1,9 +1,7 @@
 use bevy::{
     ecs::{
-        query::Changed,
-        system::{Query, ResMut},
-    },
-    ui::Interaction,
+        event::EventReader, query::Changed, system::{Query, ResMut}
+    }, input::{mouse::{MouseButton, MouseButtonInput}, ButtonState}, ui::Interaction
 };
 
 use crate::{
@@ -14,6 +12,7 @@ use crate::{
 
 pub fn select_animal_button(
     select_animal_button_queries: Query<(&SelectAnimalButton, &Interaction), Changed<Interaction>>,
+    mut mouse_button_input_events: EventReader<MouseButtonInput>,
     mut selected_item: ResMut<SelectedMenuItem>,
 ) {
     let Ok(select_animal_button_query) = select_animal_button_queries.get_single() else {
@@ -22,19 +21,26 @@ pub fn select_animal_button(
 
     match *select_animal_button_query.1 {
         Interaction::Pressed => {
-            selected_item.menu_selection = MainMenuSelection::Animals;
-            // TODO change this to None and get it from the sub menu
-            selected_item.animal_selection = ZooAnimal::Chicken;
-            selected_item.terrain_selection = WorldTerrain::None;
+          
         }
         Interaction::Hovered => {
-            selected_item.menu_selection = MainMenuSelection::Animals;
-            selected_item.animal_selection = ZooAnimal::None;
-            selected_item.terrain_selection = WorldTerrain::None;
+            // for mouse_button_event in mouse_button_input_events.read() {
+            //     if mouse_button_event.button != MouseButton::Left {
+            //         return;
+            //     }
+            //     if mouse_button_event.state != ButtonState::Pressed {
+            //         return;
+            //     }
+                
+                selected_item.menu_selection = MainMenuSelection::Animals;
+                // TODO change this to None and get it from the sub menu
+                selected_item.animal_selection = ZooAnimal::Chicken;
+                selected_item.terrain_selection = WorldTerrain::None;
+            // }
         }
         Interaction::None => {
             selected_item.menu_selection = MainMenuSelection::None;
-            selected_item.animal_selection = ZooAnimal::None;
+            // selected_item.animal_selection = ZooAnimal::None;
             selected_item.terrain_selection = WorldTerrain::None;
         }
     }
