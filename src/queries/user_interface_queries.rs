0189bf2b543d5a42fade400efd87b033fa981943
HoppySophie <82888894+HoppySophie@users.noreply.github.com>
2024-04-13 15:08:27 +0100

    update

--- src/queries/user_interface_queries.rs
+++ src/queries/user_interface_queries.rs
@@ -1,9 +1,9 @@
 use bevy::ecs::{entity::Entity, query::QueryData};
 
-use crate::components::menu::UserInterface;
+use crate::components::menu::SelectionMenu;
 
 #[derive(QueryData)]
 pub struct UserInterfaceEntityQuery {
     pub entity: Entity,
-    pub user_interface: &'static UserInterface,
+    pub user_interface: &'static SelectionMenu,
 }
--- src/systems/user_interface/layout/spawn_selection_menu.rs
+++ src/systems/user_interface/layout/spawn_selection_menu.rs
@@ -1,17 +1,46 @@
+use bevy::{
+    asset::AssetServer, ecs::system::{Commands, Res}, hierarchy::BuildChildren, render::color::Color, ui::{node_bundles::NodeBundle, Display, GridTrack, PositionType, Style, Val}
+};
 
+use crate::components::{constants::TILE_SIZE, menu::SelectionMenu};
 
-use bevy::{asset::AssetServer, ecs::{system::{Commands, Res}}, ui::{node_bundles::NodeBundle}};
+pub fn spawn_selection_menu(mut commands: Commands, _asset_server: Res<AssetServer>) {
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                display: Display::Grid,
+                grid_template_columns: vec![GridTrack::flex(1.0)],
+                grid_template_rows: vec![
+                    GridTrack::flex(1.0),
+                    GridTrack::flex(1.0),
+                    GridTrack::flex(1.0),
+                    GridTrack::flex(1.0),
+                    GridTrack::flex(1.0),
+                    GridTrack::flex(1.0),
+                ],
+                width: Val::Px(TILE_SIZE * 2.0),
+                height: Val::Px(TILE_SIZE * 6.0 * 2.0),
+                position_type: PositionType::Absolute,
+                left: Val::Percent(0.0),
+                top: Val::Percent(0.0),
+                ..Default::default()
+            },
+            background_color: Color::rgba(0.0, 0.0, 0.0, 0.0).into(),
+            ..Default::default()
+        })
+        .insert(SelectionMenu)
+        .with_children(|parent| {
+            // Animals
+            
 
+            // Fences
 
+            // Terrain
 
-pub fn spawn_selection_menu(
-    mut commands: Commands,
-    _asset_server: Res<AssetServer>,
-) {
-    commands.spawn(
-        NodeBundle {
-        ..Default::default()
-        }
-    );
+            // Trees
+
+            // Rocks
+
+            // Shelter
+        });
 }
-
