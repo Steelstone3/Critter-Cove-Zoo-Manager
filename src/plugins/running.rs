1e25be8fb209bceb06b5b68e46611e39cd54663d
Skippy <steelstone3@gmail.com>
2024-04-14 01:02:03 +0100

    basic but working

--- src/plugins/running.rs
+++ src/plugins/running.rs
@@ -23,18 +23,22 @@ pub struct RunningPlugin;
 
 impl Plugin for RunningPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, camera_movement.after(select_animal_button))
-            .add_systems(Update, camera_position_reset.after(select_animal_button))
-            .add_systems(Update, camera_zoom_keyboard.after(select_animal_button))
-            .add_systems(
-                Update,
-                camera_zoom_mouse_and_touchpad.after(select_animal_button),
-            )
-            .add_systems(Update, play_music)
-            .add_systems(Update, spawn_selection_menu)
-            .add_systems(Update, select_animal_button)
-            .add_systems(Update, animate_sprites)
-            .add_systems(Update, spawn_animal.after(select_animal_button))
-            .add_systems(Update, animal_movement);
+        app
+        .add_systems(Update, select_animal_button);
+        
+        
+        // .add_systems(Update, camera_movement.after(select_animal_button))
+        //     .add_systems(Update, camera_position_reset.after(select_animal_button))
+        //     .add_systems(Update, camera_zoom_keyboard.after(select_animal_button))
+        //     .add_systems(
+        //         Update,
+        //         camera_zoom_mouse_and_touchpad.after(select_animal_button),
+        //     )
+        //     .add_systems(Update, play_music)
+        //     .add_systems(Update, spawn_selection_menu)
+        //     .add_systems(Update, select_animal_button)
+        //     .add_systems(Update, animate_sprites)
+        //     .add_systems(Update, spawn_animal.after(select_animal_button))
+        //     .add_systems(Update, animal_movement);
     }
 }
