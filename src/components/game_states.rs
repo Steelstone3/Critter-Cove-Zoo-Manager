70922c64f5a1e03feb7157b50345e6a5c2fb0e93
Skippy <steelstone3@gmail.com>
2024-04-23 02:18:09 +0100

    update

--- src/components/game_states.rs
+++ src/components/game_states.rs
@@ -1,10 +1,9 @@
 use bevy::ecs::schedule::States;
 
 #[allow(dead_code)]
-#[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum GameState {
     MainMenu,
-    #[default] // TODO move this to main menu
     Playing,
     Paused,
 }
