ccfd64fc2121812d35b9cc2c7282442b489908c7
Skippy <steelstone3@gmail.com>
2024-04-24 00:09:09 +0100

    update

--- src/systems/user_interface/interactions/select_fence_button.rs
+++ src/systems/user_interface/interactions/select_fence_button.rs
@@ -10,28 +10,27 @@ use bevy::{
 
 use crate::{
     events::user_interface_event::UserInterfaceEvent,
-    queries::user_interface_queries::{ButtonFilters, SelectAnimalButtonQuery},
+    queries::user_interface_queries::{ButtonFilters, SelectFenceButtonQuery},
     resources::selected_item::SelectedMenuItem,
     systems::user_interface::interactions::main_menu_selection::MainMenuSelection,
 };
 
 pub fn select_fence_button(
-    mut select_animal_button_queries: Query<SelectAnimalButtonQuery, ButtonFilters>,
+    mut select_fence_button_queries: Query<SelectFenceButtonQuery, ButtonFilters>,
     mut selected_item: ResMut<SelectedMenuItem>,
     mut user_interface_event: EventWriter<UserInterfaceEvent>,
 ) {
-    let Ok(mut select_animal_button_query) = select_animal_button_queries.get_single_mut() else {
+    let Ok(mut select_fence_button_query) = select_fence_button_queries.get_single_mut() else {
         return;
     };
 
-    match *select_animal_button_query.interaction {
+    match *select_fence_button_query.interaction {
         Interaction::Pressed => {
             tracing::info!("Pressed Fence");
 
             SelectedMenuItem::reset(&mut selected_item);
-            selected_item.menu_selection = MainMenuSelection::Animals;
-            selected_item.animal_selection =
-                select_animal_button_query.selected_animal_button.animal;
+            selected_item.menu_selection = MainMenuSelection::Fences;
+            selected_item.fence_selection = select_fence_button_query.selected_fence_button.fence;
 
             user_interface_event.send(UserInterfaceEvent {});
         }
@@ -39,7 +38,7 @@ pub fn select_fence_button(
             tracing::info!("Hovered Fence");
         }
         Interaction::None => {
-            *select_animal_button_query.border_color = Color::DARK_GRAY.into();
+            *select_fence_button_query.border_color = Color::DARK_GRAY.into();
         }
     }
 }
--- src/systems/user_interface/interactions/select_path_menu_button.rs
+++ src/systems/user_interface/interactions/select_path_menu_button.rs
@@ -1,3 +1,9 @@
+use crate::{
+    events::user_interface_event::UserInterfaceEvent,
+    queries::user_interface_queries::{ButtonFilters, SelectPathMenuButtonQuery},
+    resources::selected_item::SelectedMenuItem,
+    systems::user_interface::interactions::main_menu_selection::MainMenuSelection,
+};
 use bevy::{
     ecs::{
         event::EventWriter,
@@ -8,42 +14,34 @@ use bevy::{
     utils::tracing,
 };
 
-use crate::{
-    events::user_interface_event::UserInterfaceEvent,
-    queries::user_interface_queries::{ButtonFilters, SelectAnimalMenuButtonQuery},
-    resources::selected_item::SelectedMenuItem,
-    systems::user_interface::interactions::main_menu_selection::MainMenuSelection,
-};
-
 pub fn select_path_menu_button(
-    mut select_animal_menu_button_queries: Query<SelectAnimalMenuButtonQuery, ButtonFilters>,
+    mut select_path_menu_button_queries: Query<SelectPathMenuButtonQuery, ButtonFilters>,
     mut selected_item: ResMut<SelectedMenuItem>,
     mut user_interface_event: EventWriter<UserInterfaceEvent>,
 ) {
-    let Ok(mut select_animal_menu_button_query) =
-        select_animal_menu_button_queries.get_single_mut()
+    let Ok(mut select_path_menu_button_query) = select_path_menu_button_queries.get_single_mut()
     else {
         return;
     };
 
-    match *select_animal_menu_button_query.interaction {
+    match *select_path_menu_button_query.interaction {
         Interaction::Pressed => {
             tracing::info!("Pressed Path");
 
             SelectedMenuItem::reset(&mut selected_item);
             selected_item.menu_selection = MainMenuSelection::Paths;
 
-            *select_animal_menu_button_query.border_color = Color::YELLOW.into();
+            *select_path_menu_button_query.border_color = Color::YELLOW.into();
 
             user_interface_event.send(UserInterfaceEvent {});
         }
         Interaction::Hovered => {
             tracing::info!("Hovered Path");
 
-            *select_animal_menu_button_query.border_color = Color::YELLOW.into();
+            *select_path_menu_button_query.border_color = Color::YELLOW.into();
         }
         Interaction::None => {
-            *select_animal_menu_button_query.border_color = Color::DARK_GRAY.into();
+            *select_path_menu_button_query.border_color = Color::DARK_GRAY.into();
         }
     }
 }
