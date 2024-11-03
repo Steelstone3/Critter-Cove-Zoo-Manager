78b90c7723d2153efee823b80927a301fd5c689f
Skippy <steelstone3@gmail.com>
2024-11-03 22:15:07 +0000

    version-03-11-24

--- src/systems/user_interface/layouts/spawn_menu.rs
+++ src/systems/user_interface/layouts/spawn_menu.rs
@@ -161,6 +161,70 @@ pub fn spawn_menu(mut contexts: EguiContexts, mut selected_menu_item: ResMut<Sel
                     selected_menu_item.reset();
                     selected_menu_item.path_selection = PathSprite::Path1;
                 }
+                if ui.add(egui::Button::new("Path 2")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path2;
+                }
+                if ui.add(egui::Button::new("Path 3")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path3;
+                }
+                if ui.add(egui::Button::new("Path 4")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path4;
+                }
+                if ui.add(egui::Button::new("Path 5")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path5;
+                }
+                if ui.add(egui::Button::new("Path 6")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path6;
+                }
+                if ui.add(egui::Button::new("Path 7")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path7;
+                }
+                if ui.add(egui::Button::new("Path 8")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path8;
+                }
+                if ui.add(egui::Button::new("Path 9")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path9;
+                }
+                if ui.add(egui::Button::new("Path 10")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path10;
+                }
+                if ui.add(egui::Button::new("Path 11")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path11;
+                }
+                if ui.add(egui::Button::new("Path 12")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path12;
+                }
+                if ui.add(egui::Button::new("Path 13")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path13;
+                }
+                if ui.add(egui::Button::new("Path 14")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path14;
+                }
+                if ui.add(egui::Button::new("Path 15")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path15;
+                }
+                if ui.add(egui::Button::new("Path 16")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path16;
+                }
+                if ui.add(egui::Button::new("Path 17")).clicked() {
+                    selected_menu_item.reset();
+                    selected_menu_item.path_selection = PathSprite::Path17;
+                }
             });
         }
     }
