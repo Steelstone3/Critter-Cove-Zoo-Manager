76dc82ef3320262143ea7d01d2f70611bf992d6e
Skippy <steelstone3@gmail.com>
2025-02-27 11:54:15 +0000

    version-27-02-25

--- src/systems/animate_sprites.rs
+++ src/systems/animate_sprites.rs
@@ -10,14 +10,11 @@ pub fn animate_sprites(time: Res<Time>, mut animations_query: Query<MutableAnima
         animation_query.animation_timer.timer.tick(time.delta());
 
         if animation_query.animation_timer.timer.just_finished() {
-            match animation_query.sprite.texture_atlas.as_mut() {
-                Some(texture_atlas) => {
-                    texture_atlas.index += 1;
-                    if texture_atlas.index >= animation_query.animation_timer.frame_count {
-                        texture_atlas.index = 0;
-                    }
+            if let Some(texture_atlas) = animation_query.sprite.texture_atlas.as_mut() {
+                texture_atlas.index += 1;
+                if texture_atlas.index >= animation_query.animation_timer.frame_count {
+                    texture_atlas.index = 0;
                 }
-                None => {}
             }
         }
     }
